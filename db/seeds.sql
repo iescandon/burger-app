f300dc237c4de9eacb120a4b46359c0047ea6596
Inez Escandon <iescan4@gmail.com>
2020-07-13 14:17:25 -0500

    created db folder with scheme and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Double-cheese Burger", false),
+("Blackbean Veggie Burger", false),
+("BBQ Bacon Burger", false);
+
+SELECT * FROM burgers;
